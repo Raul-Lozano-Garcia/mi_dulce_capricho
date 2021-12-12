fe1013cec517304c2561c10f91ae4569d9137d7a
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2021-12-12 19:42:00 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,3 +9,55 @@ boton.addEventListener("click",
         nav.classList.toggle('mostrar');
         toggle.classList.toggle('mostrar');
     });
+
+
+    
+const filtros = document.querySelectorAll('.filtro');
+
+filtros.forEach(filtro => { 
+
+  filtro.addEventListener('click', 
+    ()=> {
+
+        const filtroSeleccionado = filtro.getAttribute('data-filter');
+        let tartasEsconder = document.querySelectorAll(`.pastel:not([data-filter='${filtroSeleccionado}'])`);
+        let tartasMostrar = document.querySelectorAll(`.pastel[data-filter='${filtroSeleccionado}']`);
+
+        if (filtroSeleccionado == 'todas') {
+            tartasEsconder = [];
+            tartasMostrar = document.querySelectorAll('.pastel[data-filter]');
+        }
+
+        tartasEsconder.forEach(tarta => {
+        tarta.classList.add('esconder');
+        tarta.classList.remove('mostrar');
+        });
+
+        tartasMostrar.forEach(tarta => {
+        tarta.classList.remove('esconder');
+        tarta.classList.add('mostrar'); 
+        });
+
+  });
+});
+
+
+const pasteles=document.querySelectorAll(".pastel");
+const modal=document.querySelector(".comprar");
+const salir=document.querySelector(".comprar span");
+
+pasteles.forEach(
+    (pastel)=>{
+        pastel.addEventListener("click",
+        ()=>{
+            modal.classList.remove("no_ver");
+            modal.classList.add("ver");
+        });
+    }
+);
+
+salir.addEventListener("click",
+()=>{
+    modal.classList.remove("ver");
+    modal.classList.add("no_ver");
+});
